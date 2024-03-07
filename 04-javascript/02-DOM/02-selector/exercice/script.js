74d920fcd4279ce6e2ca4746f5f5054667b31474
Nolwenn <you@example.com>
2024-03-07 09:42:56 +0100

    selecteur JS

---
+++ 04-javascript/02-DOM/02-selector/exercice/script.js
@@ -0,0 +1,8 @@
+// ------------EXO 1 ---------------
+// Déplacer la modale dans le body.
+
+// ----------- EXO 2 ---------------
+// modifier le texte des 3 li du footer, si possible avec une boucle.
+
+// ------------ EXO 3 --------------
+// Remplacer le texte des paragraphes pair.
