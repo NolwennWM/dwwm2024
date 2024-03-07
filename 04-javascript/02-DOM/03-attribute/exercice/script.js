a05fd44c4735fc4c7d5706a62c5874d6ddffb82b
Nolwenn <nolwenn.wm@afci-formation.fr>
2024-03-07 15:33:58 +0100

    DOM event part 1 + exercice

--- 04-javascript/02-DOM/03-attribute/exercice/script.js
+++ 04-javascript/02-DOM/03-attribute/exercice/script.js
@@ -1,15 +1,33 @@
 /* 
     Exercice 1 :
-    Changer la taille de chaque paragraphe du main.
+    Changer la taille de la police de chaque paragraphe du main.
     chaque paragraphe doit être plus gros que le précédent.
 */
-
+const paragraphe = document.querySelectorAll("main p");
+for(let i = 0; i < paragraphe.length; i++)
+{
+    paragraphe[i].style.fontSize = (1+i)+ "rem";
+}
 /* 
     Exercice 2 :
     Faite apparaître la modale via une transition depuis la gauche. 
 */
-
+const modale = document.getElementsByTagName("aside")[0];
+modale.style.transition = "left 1s linear";
+modale.style.left = "50%";
 /* 
     Exercice 3 :
     Faite que la couleur de fond de la modale soit aléatoire à chaque rechargement de la page.
 */
+const dv = document.querySelector("div");
+
+function bgColor()
+{
+    var x = Math.floor(Math.random()*256);
+    var y = Math.floor(Math.random()*256);
+    var z = Math.floor(Math.random()*256);
+    var bg = "rgb(" + x + ","+y+","+z+")";
+    // var bg = `rgb(${x}, ${y}, ${z})`
+    dv.style.backgroundColor = bg;
+}
+bgColor();
---
+++ 04-javascript/02-DOM/04-event/exercice/script.js
@@ -0,0 +1,31 @@
+/*
+    Exercice 1 :
+
+    Faire que lors de la selection d'une couleur dans la div 2
+    le texte du bouton change de couleur, 
+    et lors de l'appuie sur le bouton, 
+    le background de la div change de couleur.
+*/
+
+/* 
+    Exercie 2 :
+
+    Lors du clique sur le bouton de la div 3,
+    faire apparaître une modale (soit déjà créé en html/ soit que l'on crée en JS)
+    Cette modale doit contenir un élément permettant de la faire disparaître.
+*/
+
+/* 
+    Exercice 3 :
+
+    Faites que tous nos li dans la nav double de taille lorsque l'on clique dessus.
+    puis retournent à leurs tailles d'origine si on clique de nouveau dessus.
+*/
+
+/* 
+    Exercie 4 :
+    
+    Utilise les évènements "mouseenter" et "mousemove" pour 
+    faire que lorsque l'on passe sur le span du footer, il commence à suivre la souris
+    et cela jusqu'à ce que l'on clique, il retournera alors à sa position d'origine.
+*/
