e694fa226593c1a4aa03de67efc4bc59946d3d88
Nolwenn <you@example.com>
2024-03-07 13:05:16 +0100

    attribute

--- 04-javascript/02-DOM/02-selector/exercice/script.js
+++ 04-javascript/02-DOM/02-selector/exercice/script.js
@@ -1,8 +1,32 @@
 // ------------EXO 1 ---------------
 // Déplacer la modale dans le body.
 
+const div = document.querySelector("div");
+const body = document.querySelector('body');
+// document.body.appendChild(div);
+body.appendChild(div);
+
 // ----------- EXO 2 ---------------
 // modifier le texte des 3 li du footer, si possible avec une boucle.
+// const test = document.querySelector('footer li');
+for (let i = 0; i < lis.length; i++) 
+{
+    lis[i].textContent = "Changé";
+}
 
 // ------------ EXO 3 --------------
 // Remplacer le texte des paragraphes pair.
+
+// Solution 1 :
+const paragraphes = document.querySelectorAll("main p");
+
+paragraphes.forEach((paragraphes,index)=>{
+    if(index % 2 !== 0)
+    {
+        paragraphes.textContent = "Nouveau texte";
+    }
+});
+// Solution 2 :
+const paragraphes2 = document.querySelectorAll('main p:nth-child(even)');
+console.log(paragraphes2);
+// refaire une boucle 
