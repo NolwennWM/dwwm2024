9b8e9a2e33c118c9173adf8403bc4287d93b1603
Nolwenn <nolwenn.wm@afci-formation.fr>
2024-03-11 11:59:08 +0100

    debut cours regex

---
+++ 04-javascript/04-regex/exercice/script.js
@@ -0,0 +1,24 @@
+/*
+    1.Le champ nom d'utilisateur doit tourner au rouge si 
+    il contient autre chose que des lettres, - ou _;
+
+    2.Le champ téléphone doit passer au rouge si le ce qui est entré
+    ne correspond pas à un numéro de téléphone.
+
+    3.Le champ email doit passer au rouge si ce qui est entré ne 
+    correspond pas à un email.
+
+    4.Ajouter une barre de progression qui change de couleur
+    et se rempli à chaque fois que l'utilisateur sécurise 
+    un peu plus sont mdp :
+        -lettre minuscule.
+        -lettre majuscule.
+        -chiffre.
+        -caractère spécial.
+        -au moins 8 caractère.
+        
+    5. le champ mdp bis doit tourner au rouge si il ne correspond 
+    pas au champ mdp.
+    (le changement au rouge peut être personalisé autrement,
+    l'important est de montrer à l'utilisateur qu'il se trompe)
+ */
