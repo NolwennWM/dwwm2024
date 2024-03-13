7b4347d1febebeb70162bd5cb3de8afe40c2b266
Nolwenn <you@example.com>
2024-03-13 11:49:52 +0100

    cours observer et exercice

---
+++ 04-javascript/05-observer/exercice/script.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+ * ---------------- EXO 1 --------------------
+ * 1. Rendre tous les paragraphes du main, invisible.
+ * 2. Ajouter Une observation sur chaque paragraphes.
+ * 3. Lorsque l'élément est au moins à moitié dans le viewport, le rendre visible.
+ * 4. Désactiver la détection de l'élément une fois l'action terminé.
+ * (Bonus). Faire venir le paragraphe depuis le côté.
+ * ---------------- EXO 2 ----------------------
+ * 1. Lorsque le dernier paragraphe est à 200px en dessous du viewport.
+ *      Créer 10 paragraphes et les ajouter à la suite du main.
+ * 2. Désactiver la détection du précédent dernier paragraphe.
+ * 3. Ajouter l'animation de l'exercice 1 aux nouveaux paragraphes.
+ * 4. Ajouter la détection du dernier paragraphe au nouveau dernier paragraphe.
+ */
--- 04-javascript/05-observer/script.js
+++ 04-javascript/05-observer/script.js
@@ -2,5 +2,93 @@
 const 
     indicator = document.querySelector(".scroll-indicator"),
     main = document.querySelector("main"),
-    options = {};
+    options = {
+        /* 
+            Dans quel élément l'observation se fait, 
+            par défaut le viewport, mais on pourrait détecter l'intersection dans un autre élément.
+        */
+        // root: main
+        /* 
+            permet de changer à partir d'où se fait la détection
+            une valeur positive provoquera la détection avant l'entré dans le viewport
+            Une valeur négative le fera après sont arrivé dans le viewport
+        */
+        // rootMargin: "50px"
+        /* 
+            Indique le pourcentage visible de l'élément (entre 0 et 1)
+            qui doit être dans le viewport pour déclencher l'action.
+        */
+        // threshold: 0.05
+    };
     
+/* 
+    Les observers sont des objets permetant un peu comme un écouteur d'évènement, 
+    d'attendre une action dans la page pour lancer une fonction.
+
+    On trouvera par exemple le mutation observer qui attend un changement dans le HTML.
+
+    Ou celui qu'on testera ici, l'intersection observer qui attend qu'un élément HTML entre ou sort du viewport.
+
+    Dans ce premier exemple on activera un "addEventListener" quand le main entre dans le viewport et on le retirera quand il sortira du viewport.
+    Cela peu servir à alléger la page lorsqu'un évènement ne sert à rien.
+
+    L'intersection observer se présente comme une classe à instancier avec en premier paramètre, une fonction callback et en second, des possibles options sous forme d'objet.
+*/
+const observer = new IntersectionObserver(setIndicator, options);
+/* 
+    L'observer a besoin qu'on lui indiquer quel élément HTML il doit observer :
+*/
+observer.observe(main);
+/* 
+    La fonction callback sera lancé à chaque fois qu'un élément obersé entre ou quitte le viewport.
+    Elle va récupérer en argument, un tableau contenant une entré pour chaque élément observé entre ou quittant le viewport.
+*/
+function setIndicator(entries)
+{
+    console.log(entries);
+    /* 
+        Au chargement de la page, la fonction est appelé une première fois,
+        Cela pour indiquer si les éléments observés sont déjà présent ou non.
+        Dans notre cas, on observe qu'un seul élément, donc pour éviter de travailler avec un tableau, j'utilise une nouvelle variable :
+    */
+    const entry = entries[0];
+
+    console.log(entry);
+
+    if(entry.isIntersecting)
+    {
+        window.addEventListener("scroll", indicatorAnimation);
+    }
+    else
+    {
+        window.removeEventListener("scroll", indicatorAnimation);
+    }
+}
+/* 
+    Si on souhaite arrêté une observation, on peut utiliser :
+    observer.unobserve(main)
+    Si on souhaite supprimer toute les observations :
+    observer.disconnect()
+*/
+function indicatorAnimation()
+{
+    console.log("SCROOOLLL!!");
+    /* 
+        scrollY représente le nombre de pixel scrollé
+        offsetTop la position de l'élément par rapport au haut de la page
+    */
+    if(window.scrollY > main.offsetTop)
+    {
+        /* 
+            "scrollHeight" la hauteur de l'élément incluant le padding vertical.
+            "nombre.toFixed(n)" retourne un string d'un nombre avec "n" chiffre après la virgule
+        */
+        const prc = ((window.scrollY - main.offsetTop)/main.scrollHeight).toFixed(2);
+        console.log(prc);
+        indicator.style.transform = `scale(${prc})`;
+    }
+    else
+    {
+        indicator.style.transform = "";
+    }
+}
