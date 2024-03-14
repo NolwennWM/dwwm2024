bd7e9f7aad2fab511eb38a4eaddff7833d3d5ff4
Nolwenn <you@example.com>
2024-03-14 14:31:15 +0100

    json et exercice todolist

---
+++ 04-javascript/07-json/exercice/todoscript.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * 1. Créer une todo list. à chaque appui sur le bouton ajout,
+ * créer un nouvel élément dans la liste.
+ * cet élément doit contenir la valeur de l'input et une croix.
+ * On en profitera pour vider l'input.
+ * 2. le clique sur un élément de la liste lui ajoutera une classe qui aura pour 
+ * effet de barrer l'élément.
+ * 3. le clique sur la croix supprimera l'élément concerné.
+ * 4. sauvegarder la liste en localstorage.
+ * 5. afficher la liste sauvegardé au chargement de la page.
+ * 6. éditer la liste lorsque l'on coche ou supprime un élément.
+ * Bonus : Utiliser le drag and drop pour déplacer nos éléments dans la liste. il faudra penser à sauvegarder les éléments déplacé.
+ */
