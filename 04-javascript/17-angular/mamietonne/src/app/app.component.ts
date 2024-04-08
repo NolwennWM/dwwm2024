3adb00c3543b085f969c14f22fdf383829096f6f
Nolwenn <nolwenn.wm@afci-formation.fr>
2024-04-08 11:59:37 +0200

    angular component et template

--- 04-javascript/17-angular/mamietonne/src/app/app.component.ts
+++ 04-javascript/17-angular/mamietonne/src/app/app.component.ts
@@ -1,24 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RECETTES } from './RecetteList';
+import { Recette } from './Recette';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'Super mamietonne ';
-  recetteList = ["Carbonade", "Okonomiyaki", "Cannelé"];
+  recetteList: Recette[] = RECETTES;
+  recetteSelected: Recette|undefined;
 
   ngOnInit(): void
   {
     console.table(this.recetteList);    
-    this.selectRecette(this.recetteList[0]);
+    // this.selectRecette(this.recetteList[0]);
   }
-  selectRecette(nom:string): void
+  selectRecette(recetteId: string): void
   {
-    console.log(`Vous avez sélectionné ${nom}`);    
+    const index: number = parseInt(recetteId);
+    const recette: Recette|undefined = this.recetteList.find(rec=>rec.id === index);
+    if(recette)
+    {
+      console.log(`Vous avez sélectionné ${recette.name}`);   
+    }
+    else
+    {
+      console.log("Aucune recette correspondante");
+    }
+    this.recetteSelected = recette;
   }
 }
