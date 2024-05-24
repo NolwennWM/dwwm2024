76bd6e30ba6c6f667fb139ba725d9c03c4ebf6c5
Nolwenn <nolwenn.wm@afci-formation.fr>
2024-05-24 10:20:48 +0200

    debut create

--- 07-php/03-crud/01-create.php
+++ 07-php/03-crud/01-create.php
@@ -1,4 +1,30 @@
 <?php
+/* 
+    CRUD signifie :
+        Create
+        Read
+        Update
+        Delete
+    En règle général, chaque table de la BDD doit pouvoir être lu, se voir créer une donnée, être mis à jour et pouvoir supprimer une donnée.
+
+    On ne doit pas pouvoir aller sur la page d'inscription si on est déjà connecté :
+*/
+require "../ressources/service/_shouldBeLogged.php";
+shouldBeLogged(false, "./02-read.php");
+
+$username = $email = $password = "";
+$error = [];
+$regexPass = "/^(?=.*[!?@#$%^&*+-])(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{6,}$/";
+
+if($_SERVER['REQUEST_METHOD']==='POST' && isset($_POST['inscription']))
+{
+    // On inclu notre service de connexion, et nos sécurités :
+    require "../ressources/service/_pdo.php";
+    require "../ressources/service/_csrf.php";
+
+    $pdo = connexionPDO();
+}
+
 $title = " CRUD - Create ";
 require("../ressources/template/_header.php");
 ?>
